56602fd825ff254b5f581226657b69f963762b58
Dani-Bcn <nneodani@gmail.com>
2022-10-12 17:25:11 +0200

    Save responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React , {useState }from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+import {motion,LayoutGroup} from 'framer-motion'
 
-const Nav = () => {    
+const Nav = () => {  
+    const [x,setX] = useState("start")  
+    const [changeClass,setChangeClass] = useState("containerEnd")
+    const spring = {
+        type: "spring",
+        stiffness: 300,
+        damping:20
+      };
     return (
-        <div className='nav'>
-           <ul>
-                {<NavLink className={(element) => element.isActive ? 'selected' : ''} to="./AboutMe"><h3>Sobre mi</h3></NavLink>} 
-                {<NavLink  to="/Proyects"><h3>Proyectos</h3></NavLink>}  
-                {<NavLink  to="/Design"><h3>Diseños 3D</h3></NavLink>}                                    
-           </ul>
-        </div>
+        <motion.div className='nav'>                 
+            <ul>
+                {<NavLink onMouseOver={()=>setChangeClass("containerStart")}  to="/AboutMe"><h3>Sobre mi</h3></NavLink>} 
+                {<NavLink  onMouseOver={()=>setChangeClass("containerCenter")} to="/Proyects"><h3>Proyectos</h3></NavLink>}  
+                {<NavLink   onMouseOver={()=>setChangeClass("containerEnd")} to="/Design"><h3>Diseños 3D</h3></NavLink>}                                    
+            </ul>
+            {console.log(changeClass)}
+            <motion.div   className={changeClass}>           
+                <motion.div layout transition={spring} className='underLine'></motion.div>    
+            </motion.div>   
+        </motion.div>
     );
 }
 export default Nav;
--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -6,54 +6,40 @@ import CloudsShips from './CloudsShips'
 
 const Proyects = () => {
     const slide=[<CloudsShips/>,<EasyBrick/>,<HomeWork/>]
-    const [displayLeft,setDisplayLeft] = useState(0)
-    const [displayRight,setDisplayRight] = useState(0)
-    const [count, setCount] = useState(1)
+    const [displayLeft,setDisplayLeft]  = useState("flex")
+    const [displayRight, setDisplayRight] = useState("flex")
+    const [count, setCount] = useState(0)
     useEffect(()=>{
-        if(count < 1 ){
-            setDisplayLeft(-250)
-        }else{
-            setDisplayLeft(0)
-        }
         if(count > 1 ){
-            setDisplayRight(250)
+            setDisplayLeft("none")
         }else{
-            setDisplayRight(0)
+            setDisplayLeft("flex")
         }
-       
-    })
- 
-
-   
-          const slideRight=(()=>{
+        if(count < 1 ){
+            setDisplayRight("none")
+        }else{
+            setDisplayRight("flex")
+        }       
+    })   
+        const slideRight=(()=>{
         setCount(conut => count +1)
         }) 
         const slideLeft=(()=>{
             setCount(conut => count -1)
         })   
     return (     
-        <div className='container'>
-            <h1>Proyectos</h1>      
+        <div className='container'>   
             <span>
                 <motion.button className='btnLeft' onClick={()=>slideLeft()}
-                    animate={{
-                       x:displayLeft,
-                        opacity:[0,1]                        
-                    }}
-                    transition={{ 
-                        type: "spring", 
-                        stiffness: 50
-                    }}
+                    animate={{             
+                        display:["flex",displayRight],               
+                    }}                   
                 ></motion.button>
                 {slide[count]}   
                 <motion.button className='btnRight' onClick={()=>slideRight()}
-                    animate={{
-                        x:displayRight,
-                        opacity:[0,1]
-                    }}    transition={{ 
-                        type: "spring",                       
-                        stiffness: 50,                       
-                    }}
+                    animate={{             
+                        display:["flex",displayLeft],
+                    }}  
                 ></motion.button>
             </span>                  
         </div>       
