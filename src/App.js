cd59fe1d997390c447ea981e3bd9d8b8e1c10fa3
Dani-Bcn <nneodani@gmail.com>
2023-01-06 15:22:11 +0100

    firts commit new design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import { Routes, Route, Link, useLocation, NavLink } from "react-router-dom";
 
 import {motion, AnimatePresence} from 'framer-motion'
 import './App.css'
-import Home from './components/Home.jsx'
-import Future from './components/Future';
-import AboutFuture from './components/AboutFuture'
-import ProyectsFuture from './components/ProyectsFuture';
-import SkillsFuture from './components/SkillsFuture';
-import DesignFuture from './components/DesignFuture';
-
+import Home from './components/Home'
 
 
 const App = () => {
@@ -17,12 +11,7 @@ const App = () => {
   return (
     <AnimatePresence  className='App'  initial={true}>     
         <Routes  location={location} key={location.pathname}>
-          <Route path='/' element={<Home />} />  
-          <Route path='/future' element={<Future/>}/>  
-          <Route path='/aboutfuture' element={<AboutFuture/>}/>     
-          <Route path='/proyectsfuture' element={<ProyectsFuture/>}/>     
-          <Route path='/skillsfuture' element={<SkillsFuture/>}/>   
-          <Route path='/designfuture' element={<DesignFuture/>}/>   
+          <Route path='/' element={<Home />}/>       
         </Routes>
       </AnimatePresence>   
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,75 +1,29 @@
-import { React, useState, useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { motion as m } from 'framer-motion'
-import BackPage from './BackPage';
-import ClickFooter from './ClickFooter';
-import Footer from './Footer';
+import React from 'react';
 
 const Home = () => {
-    const navigate = useNavigate()
-    const workdsTitle = ["Welcome", "I'm Dani", "Chosse your theme"]
-    const cardsSecction = ["Pofessional", "Crazy", "Minimalist"]
-    const [isOpen, setIsOpen] = useState(false)
-    const handleValues = () => {
-        setIsOpen(!isOpen)
-    }
-  
     return (
-        <m.div className='container-pages' id="pages">
-            <m.div className='container-text'>
-                {
-                    workdsTitle.map((e, i) => (
-                        <m.h3 key={i}
-                            whileInView={{
-                                opacity: [0, 1],
-                                height: [0, 75],
-                                transition: {
-                                    duration: 1.5,
-                                    ease: "circOut"
-                                }
-                            }}
-                            exit={{
-                                opacity: [1, 0],
-                                height: [75, 0],
-                                transition: {
-                                    duration: 1.5,
-                                    ease: "circIn"
-                                }
-                            }}
-                        >{e}</m.h3>
-                    ))
-                }
-            </m.div>
-            <m.div className='container-secction'>
-                {
-                    cardsSecction.map((e, i) => (
-                        <m.div key={i} className='cards-secction' onClick={() => (navigate("/future"),setIsOpen(false))}
-                            whileInView={{
-                                opacity: [0, 1],
-                                x: [50, 0],
-                                transition: {
-                                    delay: 0.5 + i / 2.5,
-                                    duration: 1.5,
-                                    ease: "circOut"
-                                }
-                            }}
-                            exit={{
-                                opacity: [1, 0],
-                                x: [0, -100],
-                                transition: {
-                                    delay: 0.3 + i / 2.5,
-                                    duration: 1,
-                                    ease: "circIn"
-                                }
-                            }}
-                        >{e}</m.div>
-                    ))
-                }
-            </m.div>
-            <BackPage />
-            <ClickFooter handleValues={handleValues} />
-            <Footer values={isOpen} />
-        </m.div>
+        <div className='container-home'>
+            <div className='container-text-home'>
+         
+                <h2>I'm </h2>
+                <h2>Dani Pérez</h2>
+                <h4>Full Stack Developer</h4>
+            </div>
+            <div className='container-image-home'>
+                <div
+                    style={{
+                        position:"absolute",
+                        width:350,
+                        height:350,
+                        top:300,
+                        backgroundColor:"wheat",
+                        borderRadius:"100%",
+                        zIndex:"1"
+                    }}
+                ></div>
+                <div className='img-home'></div>
+            </div>
+        </div>
     );
 }
 
