d631c403b359dff2d86e49bd8e931febc0a61aa1
Dani-Bcn <nneodani@gmail.com>
2023-01-08 23:03:07 +0100

    Sticks-click navbar ,done for movile

--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import { Routes, Route, useLocation, } from "react-router-dom";
+import { motion as m,AnimatePresence } from 'framer-motion'
 import Scrollbar from 'smooth-scrollbar';
-
-import { AnimatePresence } from 'framer-motion'
 import './App.css'
 import Home from './components/Home'
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
 
 const App = () => {
   useEffect(() => {
     const options = {
       "damping": 0.03
     }
-    const navbar = document.querySelector('.container-nav')
+
     const warning = document.getElementById('warning')
     const scrollbar = Scrollbar.init(document.querySelector('html'), options)
     scrollbar.addListener(status => {
       const offset = status.offset
-      warning.style.top = 50 +offset.y + 'px'
-      navbar.style.top = offset.y + 'px'
+      warning.style.top = 50 + offset.y + 'px'
       warning.style.opacity = offset.y / 1000
       document.getElementById('home').style.opacity = 1 - offset.y / 500
-    })
-    
-    console.log("La resolución de tu pantalla es: " + window.screen.width + " x " + window.screen.height)
-  }, [])
 
+    })
+  },[])
   const location = useLocation()
-  const nameLinks = ["Linkedin", "Github", "Domestika", "Contact"]
-  const links =["https://www.linkedin.com/in/daniperezbnc/","https://github.com/Dani-Bcn","https://www.domestika.org/es/nneodani",]
+
   return (
     <div>
-      <div className='container-nav' >
-        {
-          nameLinks.map((e, i) => (
-            <a key={i} href={links[i]}><h3 >{e}</h3></a>
-          ))
-        }
-      </div>
+   
       <AnimatePresence className='App' initial={true}>
         <Routes location={location} key={location.pathname}>
           <Route path='/' element={<Home />} />
-
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </AnimatePresence>
-
-
+      <Navbar/>
       <div id='warning'
         style={{
           textAlign: "center",
           position: "absolute",
-
           left: "0px",
           width: "120px",
           height: "25px",
@@ -63,7 +52,7 @@ const App = () => {
       >Under construction !</div>
 
 
-    </div>
+    </div >
 
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,98 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import { motion as m } from 'framer-motion'
+import Scrollbar from 'smooth-scrollbar';
 
 
 const Navbar = () => {
-    const nameLinks=["Linkedin","Github","Domestika","Contact"]
+    const nameLinks = ["Linkedin", "Github", "Domestika", "Contact", " "]
+    const links = ["https://www.linkedin.com/in/daniperezbnc/", "https://github.com/Dani-Bcn", "https://www.domestika.org/es/nneodani", "/contact"]
+    const [isOpen, setIsOpen] = useState(false)
+    useEffect(() => {
+
+        const navbar = document.querySelector('.container-nav')
+        const sticks = document.querySelector('#sticks')
+        const scrollbar = Scrollbar.init(document.querySelector('html'))
+        scrollbar.addListener(status => {
+            const offset = status.offset
+            navbar.style.top = offset.y + 'px'
+            sticks.style.top = offset.y + 'px'
+        })
+    })
+    const arraySticks = [5, 5, 5]
+
+    const variantsnav = {
+        open: {
+            width: "100vw",
+            opacity:1,
+        },
+        closed: {
+            width: 50,
+            opacity:0,
+        }
+    }
+    const variantsH3 = {
+        open: {
+            fontSize: "1rem",
+        },
+        closed: {
+            fontSize: 0
+        }
+    }
+    const variantsSticks = {
+        open: {
+            rotate: 90,
+           
+        },
+        closed: {
+            rotate: 0,
+          
+        }
+    }
     return (
-        
-        <div className='container-footer'>
-            {
-                nameLinks.map((e,i)=>(
-                    <h3 key={i}>{e}</h3>
-                ))                
-            }            
+        <div>
+            <m.div className='container-nav' onClick={() => setIsOpen(!isOpen)}
+                variants={variantsnav}
+                animate={
+                    isOpen ? "open" : "closed"
+                }>
+                {
+                    nameLinks.map((e, i) => (
+                        <a key={i} href={links[i]}><m.h3
+                            variants={variantsH3}
+                            animate={
+                                isOpen ? "open" : "closed"
+                            }
+                        >{e}</m.h3></a>
+                    ))
+                }
+            </m.div>
+            <m.div id="sticks" onClick={() => setIsOpen(!isOpen)}
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    width: 40,
+                    height: 50,
+                    zIndex: 5,
+                }}
+                variants={variantsSticks}
+                animate={isOpen ? "open" : "closed"}
+            >
+                {
+                    arraySticks.map((e, i) => (
+                        <div key={i}
+                            style={{
+                                width: 20,
+                                height: 2.5,
+                                marginTop:7.5,
+                                backgroundColor: "rgb(255, 233, 205)"
+                            }}
+                        ></div>
+                    ))
+                }
+            </m.div>
         </div>
+
     );
 }
 
