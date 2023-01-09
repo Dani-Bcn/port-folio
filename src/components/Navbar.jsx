1157f0d722a0f63f8b1a72e770b23fea9ce667b9
Dani-Bcn <nneodani@gmail.com>
2023-01-09 02:48:58 +0100

    New navbar , done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,48 +4,58 @@ import Scrollbar from 'smooth-scrollbar';
 
 
 const Navbar = () => {
-    const nameLinks = ["Linkedin", "Github", "Domestika", "Contact", " "]
+    const nameLinks = ["Linkedin", "Github", "Domestika", "Contact"]
     const links = ["https://www.linkedin.com/in/daniperezbnc/", "https://github.com/Dani-Bcn", "https://www.domestika.org/es/nneodani", "/contact"]
     const [isOpen, setIsOpen] = useState(false)
     useEffect(() => {
-
+        const options = {
+            "damping": 0.03
+        }
         const navbar = document.querySelector('.container-nav')
         const sticks = document.querySelector('#sticks')
-        const scrollbar = Scrollbar.init(document.querySelector('html'))
+        const scrollbar = Scrollbar.init(document.querySelector('html'), options)
         scrollbar.addListener(status => {
             const offset = status.offset
             navbar.style.top = offset.y + 'px'
             sticks.style.top = offset.y + 'px'
+            console.log(offset.y)
         })
+
     })
     const arraySticks = [5, 5, 5]
 
     const variantsnav = {
         open: {
-            width: "100vw",
-            opacity:1,
+            x: [-500, 0],
+            opacity: [0, 1],
         },
         closed: {
-            width: 50,
-            opacity:0,
+            x: [0, -500],
+            opacity: [1, 0],
+            transition: {
+                delay: 0.5
+            }
         }
     }
     const variantsH3 = {
         open: {
-            fontSize: "1rem",
+            y: [-500, 0],
+            transition: {
+                delay: 0.5
+            }
         },
         closed: {
-            fontSize: 0
+            y: [0, -500]
         }
     }
     const variantsSticks = {
         open: {
             rotate: 90,
-           
+
         },
         closed: {
             rotate: 0,
-          
+
         }
     }
     return (
@@ -55,6 +65,7 @@ const Navbar = () => {
                 animate={
                     isOpen ? "open" : "closed"
                 }>
+                    <div className='card-nav'>
                 {
                     nameLinks.map((e, i) => (
                         <a key={i} href={links[i]}><m.h3
@@ -62,16 +73,18 @@ const Navbar = () => {
                             animate={
                                 isOpen ? "open" : "closed"
                             }
+                          
                         >{e}</m.h3></a>
                     ))
                 }
+                </div>
             </m.div>
             <m.div id="sticks" onClick={() => setIsOpen(!isOpen)}
                 style={{
                     position: "absolute",
                     top: 0,
                     right: 0,
-                    width: 40,
+                    width: 50,
                     height: 50,
                     zIndex: 5,
                 }}
@@ -82,9 +95,9 @@ const Navbar = () => {
                     arraySticks.map((e, i) => (
                         <div key={i}
                             style={{
-                                width: 20,
+                                width: "50%",
                                 height: 2.5,
-                                marginTop:7.5,
+                                marginTop: 7.5,
                                 backgroundColor: "rgb(255, 233, 205)"
                             }}
                         ></div>
